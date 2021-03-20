53e6b4e9890b2f2149df9a7648e29c61e15d55df
Jaakko Kaisanlahti <jaakko.kaisanlahti@erpg.fi>
2021-03-20 11:42:04 +0200

    Lesson 11.

--- src/lectures/part2oop/OOBasics.scala
+++ src/lectures/part2oop/OOBasics.scala
@@ -5,6 +5,17 @@ object OOBasics extends App {
   println(person.x)
   person.greet("Daniel")
   person.greet()
+
+  val author = new Writer("Charles", "Dickens", 1812)
+  val novel = new Novel("Great Expectations", 1861, author)
+
+  println(novel.authorAge)
+  println(novel.isWrittenBy)
+
+  val counter = new Counter
+  counter.increment.print
+  counter.increment.increment.increment.print
+  counter.increment(10).print
 }
 
 // constructor
@@ -22,6 +33,32 @@ class Person(name: String, val age: Int = 0) {
   // multiple constructors
   def this(name: String) = this(name, 0)
   def this() = this("John Doe")
+}
+
+class Writer(firstName: String, surname: String, val year: Int) {
+  def fullName(): String = s"$firstName $surname"
+}
+
+class Novel(name: String, yearOfRelease: Int, author: Writer) {
+  def authorAge = yearOfRelease - author.year
+  def isWrittenBy: String = author.fullName()
+  def copy(newYearOfRelease: Int) = new Novel(this.name, newYearOfRelease, this.author)
+}
+
+class Counter(value: Int = 0) {
+  def currentCount = this.value
+  def increment = new Counter(this.value + 1)
+  def decrement = new Counter(this.value - 1)
+  def increment(amount: Int): Counter = {
+    if (amount <= 0) this
+    else increment.increment(amount - 1)
+  }
+  def decrement(amount: Int): Counter = {
+    if (amount <= 0) this
+    else decrement.decrement(amount - 1)
+
+  }
+    def print = println(currentCount)
 }
 
 // Class parameters are not fields
