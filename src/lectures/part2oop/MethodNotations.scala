2eb26f865c4eeaed937ac84ee1108a40055703dc
Jaakko Kaisanlahti <jaakko.kaisanlahti@erpg.fi>
2021-03-20 12:13:08 +0200

    Lesson 13.

--- src/lectures/part2oop/MethodNotations.scala
+++ src/lectures/part2oop/MethodNotations.scala
@@ -2,15 +2,20 @@ package lectures.part2oop
 
 object MethodNotations extends App {
 
-  class Person(val name: String, favoriteMovie: String) {
+  class Person(val name: String, favoriteMovie: String, val age: Int = 0) {
     def likes(movie: String): Boolean = favoriteMovie == movie
     def +(person: Person): String = s"${this.name} is hanging out with ${person.name}"
+    def +(str: String): Person = new Person(s"$name ($str)", favoriteMovie)
+    def unary_+ : Person = new Person(name, favoriteMovie, age + 1)
     def unary_! : String = s"$name what the heck?!"
     def isAlive: Boolean = true
     def apply(): String = s"Hi, my name is $name and I like $favoriteMovie"
+    def apply(times: Int): String = s"$name watched $favoriteMovie $times times"
+    def learns(subject: String): String = s"$name learns $subject"
+    def learnScala: String = learns("Scala")
   }
 
-  val mary =  new Person("Mary", "Inception")
+  val mary =  new Person("Mary", "Inception", 21)
   println(mary.likes("Inception"))
   println(mary likes "Inception")
   // infix notation / operator notation (syntactic sugar)
@@ -40,4 +45,10 @@ object MethodNotations extends App {
   // apply
   println(mary.apply())
   println(mary())
+
+  println(mary + "rockstar")
+  println(+mary.age)
+  println(mary learns "Java")
+  println(mary learnScala)
+  println(mary(2))
 }
